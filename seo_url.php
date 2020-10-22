131af53070b51a9fc69fec2c6c0f99248e146f12
SDG1212 <sokol1294@gmail.com>
2020-10-22 18:04:24 +0300

    Add manufacturer, information url rewrite

--- seo_url.php
+++ seo_url.php
@@ -27,7 +27,7 @@ class ControllerStartupSeoUrl extends Controller {
 				} else {
 					$this->request->get['route'] = 'error/not_found';
 				}
-			} elseif ($parts[0] == 'item') {
+			} elseif ($parts[0] == 'product') {
 				$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "seo_url WHERE keyword = '" . $this->db->escape($parts[1]) . "' AND store_id = '" . (int)$this->config->get('config_store_id') . "'");
 
 				if ($query->num_rows) {
@@ -39,6 +39,30 @@ class ControllerStartupSeoUrl extends Controller {
 				} else {
 					$this->request->get['route'] = 'error/not_found';
 				}
+			} elseif ($parts[0] == 'manufacturer_id') {
+				$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "seo_url WHERE keyword = '" . $this->db->escape($parts[1]) . "' AND store_id = '" . (int)$this->config->get('config_store_id') . "'");
+
+				if ($query->num_rows) {
+					$url = explode('=', $query->row['query']);
+
+					if ($url[0] == 'manufacturer_id') {
+						$this->request->get['manufacturer_id'] = $url[1];
+					}
+				} else {
+					$this->request->get['route'] = 'error/not_found';
+				}
+			} elseif ($parts[0] == 'information_id') {
+				$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "seo_url WHERE keyword = '" . $this->db->escape($parts[1]) . "' AND store_id = '" . (int)$this->config->get('config_store_id') . "'");
+
+				if ($query->num_rows) {
+					$url = explode('=', $query->row['query']);
+
+					if ($url[0] == 'information_id') {
+						$this->request->get['information_id'] = $url[1];
+					}
+				} else {
+					$this->request->get['route'] = 'error/not_found';
+				}
 			} else {
 				foreach ($parts as $part) {
 					$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "seo_url WHERE keyword = '" . $this->db->escape($part) . "' AND store_id = '" . (int)$this->config->get('config_store_id') . "'");
@@ -46,7 +70,7 @@ class ControllerStartupSeoUrl extends Controller {
 					if ($query->num_rows) {
 						$url = explode('=', $query->row['query']);
 
-						if ($url[0] != 'category_id' && $url[0] != 'product_id') {
+						if ($url[0] != 'category_id' && $url[0] != 'product_id' && $url[0] != 'manufacturer_id' && $url[0] != 'information_id') {
 							$this->request->get['route'] = $query->row['query'];
 						}
 					} else {
@@ -62,6 +86,10 @@ class ControllerStartupSeoUrl extends Controller {
 					$this->request->get['route'] = 'product/category';
 				} elseif (isset($this->request->get['product_id'])) {
 					$this->request->get['route'] = 'product/product';
+				} elseif (isset($this->request->get['manufacturer_id'])) {
+					$this->request->get['route'] = 'product/manufacturer/info';
+				} elseif (isset($this->request->get['information_id'])) {
+					$this->request->get['route'] = 'information/information';
 				}
 			}
 		}
@@ -79,7 +107,9 @@ class ControllerStartupSeoUrl extends Controller {
 		foreach ($data as $key => $value) {
 			if (isset($data['route'])) {
 				if (($data['route'] == 'product/category' && $key == 'category_id') ||
-					($data['route'] == 'product/product' && $key == 'product_id')) {
+					($data['route'] == 'product/product' && $key == 'product_id') ||
+					(($data['route'] == 'product/manufacturer/info' || $data['route'] == 'product/product') && $key == 'manufacturer_id') ||
+					($data['route'] == 'information/information' && $key == 'information_id')) {
 					$_query = $key . '=' . (int)$value;
 				} else {
 					$_query = $data['route'];
@@ -91,7 +121,11 @@ class ControllerStartupSeoUrl extends Controller {
 					if ($data['route'] == 'product/category' && $key == 'category_id') {
 						$url .= '/category';
 					} elseif ($data['route'] == 'product/product' && $key == 'product_id') {
-						$url .= '/item';
+						$url .= '/product';
+					} elseif ($data['route'] == 'product/manufacturer/info' && $key == 'manufacturer_id') {
+						$url .= '/manufacturer';
+					} elseif ($data['route'] == 'information/information' && $key == 'information_id') {
+						$url .= '/information';
 					}
 
 					$url .= '/' . $query->row['keyword'];
